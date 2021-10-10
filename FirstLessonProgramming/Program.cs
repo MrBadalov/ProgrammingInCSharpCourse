9b922cbe657718f587b48dd9308fd96e3eb0e78e
MrBadalov <vasif.bedelov1@gmail.com>
2021-10-10 23:21:14 +0400

    hello world

---
+++ FirstLessonProgramming/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstLessonProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
