438ae2a291726375eb7ee8f048c5bdebdcc947a5
MrBadalov <vasif.bedelov1@gmail.com>
2021-11-17 15:34:50 +0400

    lesson 10 add

---
+++ lesson10/Lesson10_2/Lesson10_2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Lesson10_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SchoolPerson schoolPerson = new SchoolPerson();
+            schoolPerson.Greet();
+            Console.WriteLine("____________");
+
+            Student student = new Student();
+            student.Age = 21;
+            student.Greet();
+            student.ShowAge();
+            student.GoToClasses();
+            Console.WriteLine("____________");
+
+            Teacher teacher = new Teacher();
+            teacher.Age = 30;
+            teacher.Greet();
+            teacher.GoToClasses();
+            teacher.Explain();
+        }
+    }
+    
+
+    public class SchoolPerson
+    {
+        private int _age;
+        public string SchoolName { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value > 6 && value < 50)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    throw new Exception("you have entered the wrong age");
+                }
+            }
+        }
+        public void Greet()
+        {
+            Console.WriteLine($"Hello {Name} {Surname}");
+        }
+        public void ShowAge()
+        {
+            Console.WriteLine($"My age is  {Age}");
+        }
+        public void GoToClasses()
+        {
+            Console.WriteLine("I am going to class.");
+        }
+    }
+
+    public class Student : SchoolPerson
+    {
+        public new void GoToClasses()
+        {
+            Console.WriteLine($"I’m {Name} {Surname}, I am student and I'm going to class.");
+        }
+    }
+
+    public class Teacher : SchoolPerson
+    {
+        public void Explain()
+        {
+            Console.WriteLine("Explanation begin on subject.");
+        }
+
+        public new void GoToClasses()
+        {
+            Console.WriteLine($"I’m {Name} {Surname}, I am a teacher.");
+        }
+    }
+}
---
+++ lesson10/lesson10_1/lesson10_1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace lesson10_1
+{
+    
+
+    class Program
+    {
+        static void Main()
+        {
+            Shape[] shapes = new Shape[2];
+
+
+            shapes[0] = new Rectangle(7, 10);
+            shapes[1] = new Circle(6);
+
+
+            foreach (var var in shapes)
+            {
+                Console.WriteLine("Area of the shape: " + var.Area());
+                Console.WriteLine("Perimeter of the shape: " + var.Perimeter());
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public double part1, part2;
+
+        public Rectangle(double part1, double part2)
+        {
+            this.part1 = part1;
+            this.part2 = part2;
+        }
+
+        public override double Area()
+        {
+            return part1 * part2;
+        }
+
+        public override double Perimeter()
+        {
+            return 2 * (part1 + part2);
+        }
+    }
+
+
+    class Shape
+    {
+        public virtual double Area()
+        {
+            return 0.000;
+        }
+
+        public virtual double Perimeter()
+        {
+            return 0.000;
+        }
+    }
+    class Circle : Shape
+    {
+        public double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public override double Perimeter()
+        {
+            return 2 * Math.PI * radius;
+        }
+
+    }
+
+}
