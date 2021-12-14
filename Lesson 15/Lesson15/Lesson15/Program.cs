650c932d9574b69034286a36370d61bc7bbd4ac6
MrBadalov <vasif.bedelov1@gmail.com>
2021-12-15 03:25:56 +0400

    add

---
+++ Lesson 15/Lesson15/Lesson15/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace lesson15
+{
+    class Program
+    {
+        static void Main()
+        {
+            MyList<int> myList = new();
+
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+
+            int sum = 0;
+
+            foreach (int number in myList)
+            {
+                sum += number;
+            }
+
+            Console.WriteLine(sum);
+        }
+
+        public class MyList<T> : IEnumerator, IEnumerable
+        {
+            private List<T> _item;
+
+            public int Count
+            {
+                get
+                {
+                    return _item.Count;
+                }
+            }
+
+
+            public MyList()
+            {
+                _item = new List<T>();
+            }
+
+            public void Add(T item)
+            {
+                _item.Add(item);
+            }
+
+            public void Remove(T item)
+            {
+                _item.Remove(item);
+            }
+
+            public void Clear(T item)
+            {
+                _item.Clear();
+            }
+
+            public bool Contains(T item)
+            {
+                return _item.Contains(item);
+            }
+
+
+            int position = -1;
+
+            public bool MoveNext()
+            {
+                position++;
+                return (position < _item.Count);
+            }
+
+            public void Reset()
+            {
+                position = -1;
+            }
+
+            public object Current
+            {
+                get
+                {
+                    try
+                    {
+                        return _item[position];
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        throw new InvalidOperationException();
+                    }
+                }
+
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return this;
+            }
+        }
+    }
+}
