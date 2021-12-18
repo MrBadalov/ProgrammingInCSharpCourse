4b2c0086339b0affe14d9805284c977793b6996a
MrBadalov <vasif.bedelov1@gmail.com>
2021-12-19 03:05:53 +0400

    add

---
+++ Lesson 15/lesson15_3/lesson15_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lesson15_3
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Dictionary<string, int> people = new Dictionary<string, int>();
+
+            people.Add("Kamran", 27);
+            people.Add("Vasif", 28);
+            var one = people.GetEnumerator();
+            one.MoveNext();
+            var currentItem = one.Current;
+
+
+
+            foreach (var key in people.Keys)
+            {
+                Console.WriteLine( key);
+                break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
