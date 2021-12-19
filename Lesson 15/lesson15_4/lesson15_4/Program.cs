aa8715d5bd26ad81454e6c61c21b009c005df399
MrBadalov <vasif.bedelov1@gmail.com>
2021-12-19 19:06:04 +0400

    add

---
+++ Lesson 15/lesson15_4/lesson15_4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lesson15_4
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            List<string> processToKill = new List<string>(3);
+
+            List<string> process = new();
+
+            process.Add("Explorer.exe");
+            process.Add("Windows.exe");
+            process.Add("Something.exe");
+
+
+            foreach (var kill in process)
+            {
+                if (kill != "Explorer.exe")
+                {
+                    processToKill.Add(kill);
+                }
+            }
+
+            foreach (var kill in processToKill)
+            {
+                Console.WriteLine(kill);
+            }
+
+            Console.WriteLine(string.Format("Capacity {0}", processToKill.Capacity));
+            Console.WriteLine(string.Format("Count {0}", processToKill.Count));
+        }
+    }
+}
+
+
+
+
+
